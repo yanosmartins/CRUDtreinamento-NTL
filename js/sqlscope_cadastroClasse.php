f9810b790a1d055acee35cbb8dd953ff1a7f8890
Breno Macedo <brenomacedo404@gmail.com>
2020-12-21 15:49:12 -0300

    Inserção das telas de classe

---
+++ js/sqlscope_cadastroClasse.php
@@ -0,0 +1,176 @@
+<?php
+
+include "repositorio.php";
+include "girComum.php";
+
+$funcao = $_POST["funcao"];
+
+if ($funcao == 'gravaClasse') {
+    call_user_func($funcao);
+}
+
+if ($funcao == 'recuperaClasse') {
+    call_user_func($funcao);
+}
+
+if ($funcao == 'excluirClasse') {
+    call_user_func($funcao);
+}
+
+return;
+
+function gravaClasse()
+{
+
+    $reposit = new reposit(); //Abre a conexão.
+
+    //Verifica permissões
+    // $possuiPermissao = $reposit->PossuiPermissao("VALETRANSPORTEUNITARIO_ACESSAR|VALETRANSPORTEUNITARIO_GRAVAR");
+
+    // if ($possuiPermissao === 0) {
+    //     $mensagem = "O usuário não tem permissão para gravar!";
+    //     echo "failed#" . $mensagem . ' ';
+    //     return;
+    // }
+    session_start();
+    $reposit = new reposit();
+    $usuario = $_SESSION['login'];
+    $classe = $_POST['classe'];
+    $codigo =  validaCodigo($classe['codigo'] ?: 0);
+    $codigoClasse = validaString($classe['codigoClasse']);
+    $nomeClasse = validaString($classe['nomeClasse']);
+
+
+    $sql = "dbo.classe_Atualiza(
+        $codigo,
+        $codigoClasse,	
+        $nomeClasse,
+        $usuario
+        )";
+
+    $reposit = new reposit();
+    $result = $reposit->Execprocedure($sql);
+
+
+
+    $ret = 'sucess#';
+    if ($result < 1) {
+        $ret = 'failed#';
+    }
+    echo $ret;
+    return;
+}
+
+
+function recuperaClasse()
+{
+    if ((empty($_POST["id"])) || (!isset($_POST["id"])) || (is_null($_POST["id"]))) {
+        $mensagem = "Nenhum parâmetro de pesquisa foi informado.";
+        echo "failed#" . $mensagem . ' ';
+        return;
+    } else {
+        $id = +$_POST["id"];
+    }
+
+    $sql = "SELECT codigo, codigoClasse, nomeClasse FROM dbo.classe WHERE (0=0) AND codigo = " . $id;
+
+
+    $reposit = new reposit();
+    $result = $reposit->RunQuery($sql);
+
+    $out = "";
+    if (($row = odbc_fetch_array($result)))
+        $row = array_map('utf8_encode', $row);
+    $codigo = $row['codigo'];
+    $codigoClasse = $row['codigoClasse'];
+    $nomeClasse = $row['nomeClasse'];
+
+    $out =   $codigo . "^" .
+        $codigoClasse . "^" .
+        $nomeClasse;
+
+    if ($out == "") {
+        echo "failed#";
+        return;
+    }
+
+    echo "sucess#" . $out;
+    return;
+}
+
+
+function excluirClasse()
+{
+
+    $reposit = new reposit();
+
+    $id = $_POST["id"];
+
+    if ((empty($_POST['id']) || (!isset($_POST['id'])) || (is_null($_POST['id'])))) {
+        $mensagem = "Selecione um classe.";
+        echo "failed#" . $mensagem . ' ';
+        return;
+    }
+
+    $sql = "classe_Deleta ($id)";
+
+    $reposit = new reposit();
+    $result = $reposit->Execprocedure($sql);
+
+    if ($result < 1) {
+        echo ('failed#');
+        return;
+    }
+
+    echo 'sucess#' . $result;
+    return;
+}
+
+
+function validaString($value)
+{
+    $null = 'NULL';
+    if ($value == '')
+        return $null;
+    return '\'' . $value . '\'';
+}
+
+function validaNumero($value)
+{
+    if ($value == "") {
+        $value = 'NULL';
+    }
+    return $value;
+}
+function validaCodigo($value)
+{
+    return $value;
+}
+function validaData($value)
+{
+    if ($value == "") {
+        $value = 'NULL';
+        return $value;
+    }
+    $value = str_replace('/', '-', $value);
+    $value = date("Y-m-d", strtotime($value));
+    $value = "'" . $value . "'";
+    return $value;
+}
+
+function validaDataRecupera($value)
+{
+    if ($value == "") {
+        $value = '';
+        return $value;
+    }
+    $value = date('d/m/Y', strtotime($value));
+    return $value;
+}
+function validaVerifica($value)
+{
+    if ($value == "") {
+        $value = NULL;
+    }
+    return $value;
+}
