0eeb266414f5a88d8826557e053f0fd2c4ef52d2
Danilomld <danilomdaim@gmail.com>
2021-01-18 07:36:47 -0300

    config

--- js/config.php
+++ js/config.php
@@ -1,18 +1,17 @@
 <?php
- 
-  // Preencha de acordo com as informações reais do seu banco
-  define("SERVIDOR","DESKTOP-L1PESJT\SQLEXPRESS"); // Servidor do Banco de Dados
-  define("USUARIO","sa"); // Usuário do Banco de Dados
-  define("SENHA","123"); // Senha do Banco de Dados
-  define("BANCO","ntl"); // Nome do Banco de Dados
-  define("PORTA",""); // Porta do Banco de Dados
-  //define("SOCKET",""); // SOCKET do Banco de Dados
-  //define("EMAILPAT",""); // SOCKET do Banco de Dados
-  //define("EMAILTI",""); // SOCKET do Banco de Dados
-  //define("SERVSMTP",""); // Servidor smtp
-  //define("PORTASMTP",""); // Porta do servidor smtp
-  //define("LOGINEMAIL",""); // Login de quem vai enviar o email
-  //define("SENHAEMAIL",""); // Senha do sender do email
-  
-  
+// Preencha de acordo com as informações reais do seu bancoDESKTOP-NOMMKQ5
+define("SERVIDOR", "168.138.146.147\MSSQLSERVER,2866"); // Servidor do Banco de Dados
+define("USUARIO", "NTL"); // Usuário do Banco de Dados
+define("SENHA", "SistemaNtl2020"); // Senha do Banco de Dados
+define("BANCO", "ntl"); // Nome do Banco de Dados
+define("PORTA", ""); // Porta do Banco de Dados
+//define("SOCKET",""); // SOCKET do Banco de Dados
+//define("EMAILPAT",""); // SOCKET do Banco de Dados
+//define("EMAILTI",""); // SOCKET do Banco de Dados
+//define("SERVSMTP",""); // Servidor smtp
+//define("PORTASMTP",""); // Porta do servidor smtp
+//define("LOGINEMAIL",""); // Login de quem vai enviar o email
+//define("SENHAEMAIL",""); // Senha do sender do email
+
+
 ?>
