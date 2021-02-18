34bddce8aaef140f13904df06c1a8c9902d9f261
pedrohmonteiro <pedrohenriquemon@gmail.com>
2021-02-18 12:00:38 -0300

    solicitacoes

---
+++ mensageria_solicitacaoEmAndamentoFiltro.php
@@ -0,0 +1,274 @@
+<?php
+//initilize the page
+require_once("inc/init.php");
+
+//require UI configuration (nav, ribbon, etc.)
+require_once("inc/config.ui.php");
+
+//colocar o tratamento de permissão sempre abaixo de require_once("inc/config.ui.php");
+$condicaoAcessarOK = (in_array('SOLICITACAO_ACESSAR', $arrayPermissao, true));
+$condicaoGravarOK = (in_array('SOLICITACAO_GRAVAR', $arrayPermissao, true));
+
+if ($condicaoAcessarOK == false) {
+    unset($_SESSION['login']);
+    header("Location:login.php");
+}
+
+$esconderBtnGravar = "";
+if ($condicaoGravarOK === false) {
+    $esconderBtnGravar = "none";
+}
+
+/* ---------------- PHP Custom Scripts ---------
+
+  YOU CAN SET CONFIGURATION VARIABLES HERE BEFORE IT GOES TO NAV, RIBBON, ETC.
+  E.G. $page_title = "Custom Title" */
+
+$page_title = "Pregões Não Iniciados";
+
+/* ---------------- END PHP Custom Scripts ------------- */
+
+//include header
+//you can add your custom css in $page_css array.
+//Note: all css files are inside css/ folder
+$page_css[] = "your_style.css";
+include("inc/header.php");
+
+//include left panel (navigation)
+//follow the tree in inc/config.ui.php
+$page_nav["operacao"]["sub"]["mensageria"]["sub"]["solicitacaoEmAndamento"]["active"] = true;
+
+include("inc/nav.php");
+?>
+<!-- ==========================CONTENT STARTS HERE ========================== -->
+<!-- MAIN PANEL -->
+<div id="main" role="main">
+    <?php
+    //configure ribbon (breadcrumbs) array("name"=>"url"), leave url empty if no url
+    //$breadcrumbs["New Crumb"] => "http://url.com"
+    $breadcrumbs["Operação"] = "";
+    include("inc/ribbon.php");
+    ?>
+
+    <!-- MAIN CONTENT -->
+    <div id="content">
+
+        <!-- widget grid -->
+        <section id="widget-grid" class="">
+            <div class="row">
+                <article class="col-sm-12 col-md-12 col-lg-12 sortable-grid ui-sortable centerBox">
+                    <div class="jarviswidget" id="wid-id-1" data-widget-colorbutton="false" data-widget-editbutton="false" data-widget-deletebutton="false" data-widget-sortable="false" style="">
+                        <header>
+                            <span class="widget-icon"><i class="fa fa-cog"></i></span>
+                            <h2>Relatório de Tarefas</h2>
+                        </header>
+                        <div>
+                            <div class="widget-body no-padding">
+                                <form action="javascript:gravar()" class="smart-form client-form" id="formUsuarioFiltro" method="post">
+                                    <div class="panel-group smart-accordion-default" id="accordion">
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                <h4 class="panel-title">
+                                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseFiltro" class="">
+                                                        <i class="fa fa-lg fa-angle-down pull-right"></i>
+                                                        <i class="fa fa-lg fa-angle-up pull-right"></i>
+                                                        Filtro
+                                                    </a>
+                                                </h4>
+                                            </div>
+                                            <div id="collapseFiltro" class="panel-collapse collapse in">
+                                                <div class="panel-body no-padding">
+                                                    <fieldset>
+                                                        <div class="row" autocomplete="off">
+                                                            <section class="col col-6 col-auto">
+                                                                <label class="label" for="portal">Funcionário</label>
+                                                                <label class="select">
+                                                                    <select id="funcionario" name="funcionario">
+                                                                        <option></option>
+                                                                        <?php
+                                                                        $reposit = new reposit();
+                                                                        $sql = "SELECT codigo, nome FROM 
+                                                                        Ntl.funcionario WHERE ativo = 1 ORDER BY nome";
+                                                                        $result = $reposit->RunQuery($sql);
+                                                                        foreach ($result as $row) {
+                                                                            $codigo = +$row['codigo'];
+                                                                            $descricao = $row['nome'];
+                                                                            echo '<option value=' . $codigo . '>' . $descricao . '</option>';
+                                                                        }
+                                                                        ?>
+                                                                    </select><i></i>
+                                                            </section>
+
+                                                            <section class="col col-3">
+                                                                <label class="label">Data Solicitacao</label>
+                                                                <label class="input">
+                                                                    <i class="icon-append fa fa-calendar"></i>
+                                                                    <input id="data" name="data" autocompvare="off" type="text" data-dateformat="dd/mm/yy" class="datepicker" style="text-align: center" value="" data-mask="99/99/9999" data-mask-placeholder="-" autocompvare="new-password">
+                                                                </label>
+                                                            </section>
+                                                            <section class="col col-3">
+                                                                <label class="label" for="hora">Hora Solicitacao</label>
+                                                                <label class="input">
+                                                                    <i class="icon-append fa fa-clock-o"></i>
+                                                                    <input id="hora" name="hora" type="text" autocomplete="off" placeholder="hh:mm">
+                                                                </label>
+                                                            </section>
+                                                        </div>
+                                                        <div class="row">
+                                                            <section class="col col-6">
+                                                                <label class="label" for="projeto">Projeto</label>
+                                                                <label class="select">
+                                                                    <select id="projeto" name="projeto" class="">
+                                                                        <option></option>
+                                                                        <?php
+                                                                        $sql =  "SELECT codigo, descricao FROM Ntl.projeto where ativo = 1 order by descricao";
+                                                                        $reposit = new reposit();
+                                                                        $result = $reposit->RunQuery($sql);
+                                                                        foreach ($result as $row) {
+                                                                            $codigo = $row['codigo'];
+                                                                            $descricao = ($row['descricao']);
+                                                                            echo '<option value=' . $codigo . '>  ' . $descricao . ' </option>';
+                                                                        }
+                                                                        ?>
+                                                                    </select><i></i>
+                                                            </section>
+
+                                                            <section class="col col-3">
+                                                                <label class="label">Data Limite</label>
+                                                                <label class="input">
+                                                                    <i class="icon-append fa fa-calendar"></i>
+                                                                    <input id="dataLimite" name="dataLimite" autocompvare="off" type="text" data-dateformat="dd/mm/yy" class="datepicker " style="text-align: center" value="" data-mask="99/99/9999" data-mask-placeholder="-" autocompvare="new-password">
+                                                                </label>
+                                                            </section>
+                                                            <section class="col col-3">
+                                                                <label class="label" for="condicao">Urgente</label>
+                                                                <label class="select">
+                                                                    <select id="urgente" name="urgente" class="">
+                                                                        <option></option>
+                                                                        <option value="1">Sim</option>
+                                                                        <option value="0">Não</option>
+                                                                    </select><i></i>
+                                                            </section>
+                                                        </div>
+                                                        <div class="row">
+                                                            <section class="col col-3" >
+                                                                <label class="label" for="responsavelPregao"> Responsável</label>
+                                                                <label class="select">
+                                                                    <select id="responsavel" name="responsavel">
+                                                                        <option></option>
+                                                                        <?php
+                                                                        $sql =  "SELECT codigo, nome FROM Ntl.responsavel where ativo = 1 order by codigo";
+                                                                        $reposit = new reposit();
+                                                                        $result = $reposit->RunQuery($sql);
+                                                                        foreach ($result as $row) {
+                                                                            $codigo = $row['codigo'];
+                                                                            $nome = ($row['nome']);
+                                                                            echo '<option value=' . $codigo . '>  ' . $nome . '</option>';
+                                                                        }
+                                                                        ?>
+                                                                    </select><i></i>
+                                                            </section>
+                                                            <section class="col col-3">
+                                                                <label class="label" for="ativo">Ativo</label>
+                                                                <label class="select">
+                                                                    <select id="ativo" name="ativo">
+                                                                        <option></option>
+                                                                        <option value="1" selected>Sim</option>
+                                                                        <option value="0">Não</option>
+                                                                    </select><i></i>
+                                                            </section>
+                                                        </div>
+                                                    </fieldset>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <footer>
+                                        <button id="btnSearch" type="button" class="btn btn-primary pull-right" title="Buscar">
+                                            <span class="fa fa-search"></span>
+                                        </button>
+                                    </footer>
+                                </form>
+                            </div>
+                            <div id="resultadoBusca"></div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </section>
+        <!-- end widget grid -->
+    </div>
+    <!-- END MAIN CONTENT -->
+</div>
+<!-- END MAIN PANEL -->
+
+<!-- ==========================CONTENT ENDS HERE ========================== -->
+
+<!-- PAGE FOOTER -->
+<?php
+include("inc/footer.php");
+?>
+<!-- END PAGE FOOTER -->
+
+<?php
+//include required scripts
+include("inc/scripts.php");
+?>
+<!--script src="<?php echo ASSETS_URL; ?>/js/businessTabelaBasica.js" type="text/javascript"></script-->
+<!-- PAGE RELATED PLUGIN(S) 
+<script src="..."></script>-->
+<!-- Flot Chart Plugin: Flot Engine, Flot Resizer, Flot Tooltip -->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.cust.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.resize.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.time.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.tooltip.min.js"></script>
+
+<!-- Vector Maps Plugin: Vectormap engine, Vectormap language -->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+
+<!-- Full Calendar -->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/moment/moment.min.js"></script>
+<!--<script src="/js/plugin/fullcalendar/jquery.fullcalendar.min.js"></script>-->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/fullcalendar/fullcalendar.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/fullcalendar/locale-all.js"></script>
+
+
+<script>
+    $(document).ready(function() {
+
+        listarFiltro();
+
+        $('#hora').mask('99:99', {
+            placeholder: "hh:mm"
+        });
+
+        $('#btnSearch').on("click", function() {
+            listarFiltro();
+        });
+    });
+
+    function listarFiltro() {
+
+        var funcionario = $('#funcionario').val();
+        var data = $('#data').val();
+        var hora = $('#hora').val();
+        var projeto = $('#projeto').val();
+        var dataLimite = $('#dataLimite').val();
+        var urgente = $('#urgente').val();
+        var responsavel = $('#responsavel').val();
+        var ativo = $('#ativo').val();
+
+        $('#resultadoBusca').load('mensageria_solicitacaoEmAndamentoFiltroListagem.php?', {
+            funcionario: funcionario,
+            data: data,
+            hora: hora,
+            projeto: projeto,
+            dataLimite: dataLimite,
+            urgente: urgente,
+            responsavel: responsavel,
+            ativo: ativo
+        });
+
+    }
+</script>
