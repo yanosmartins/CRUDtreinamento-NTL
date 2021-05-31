6e1a2d8d674493dee6249ed986ff13995c9dff4e
mthdantas <matheusdealmeidadantas@hotmail.com>
2021-05-31 14:25:17 -0300

    correcao funcionario tela ASO closes #121

---
+++ funcionario_triagemFolhaDePontoGestor.php
@@ -0,0 +1,224 @@
+<?php
+//initilize the page
+require_once("inc/init.php");
+
+//require UI configuration (nav, ribbon, etc.)
+require_once("inc/config.ui.php");
+
+//colocar o tratamento de permissão sempre abaixo de require_once("inc/config.ui.php");
+$condicaoAcessarOK = (in_array('TRIAGEMPONTOELETRONICO_ACESSAR', $arrayPermissao, true));
+$condicaoGravarOK = (in_array('TRIAGEMPONTOELETRONICO_GRAVAR', $arrayPermissao, true));
+
+if ($condicaoAcessarOK == false) {
+    unset($_SESSION['login']);
+    header("Location:login.php");
+}
+
+$esconderBtnGravar = "";
+if ($condicaoGravarOK === false) {
+    $esconderBtnGravar = "none";
+}
+
+/* ---------------- PHP Custom Scripts ---------
+
+  YOU CAN SET CONFIGURATION VARIABLES HERE BEFORE IT GOES TO NAV, RIBBON, ETC.
+  E.G. $page_title = "Custom Title" */
+
+$page_title = "Triagem Ponto Eletrônico";
+
+/* ---------------- END PHP Custom Scripts ------------- */
+
+//include header
+//you can add your custom css in $page_css array.
+//Note: all css files are inside css/ folder
+$page_css[] = "your_style.css";
+include("inc/header.php");
+
+//include left panel (navigation)
+//follow the tree in inc/config.ui.php
+$page_nav['funcionario']['sub']["triagemPontoEletronico"]["active"] = true;
+
+include("inc/nav.php");
+?>
+<!-- ==========================CONTENT STARTS HERE ========================== -->
+<!-- MAIN PANEL -->
+<div id="main" role="main">
+    <?php
+    //configure ribbon (breadcrumbs) array("name"=>"url"), leave url empty if no url
+    //$breadcrumbs["New Crumb"] => "http://url.com"
+    $breadcrumbs["Cadastro"] = "";
+    include("inc/ribbon.php");
+    ?>
+
+    <!-- MAIN CONTENT -->
+    <div id="content">
+
+        <!-- widget grid -->
+        <section id="widget-grid" class="">
+            <div class="row">
+                <article class="col-sm-12 col-md-12 col-lg-12 sortable-grid ui-sortable centerBox">
+                    <div class="jarviswidget" id="wid-id-1" data-widget-colorbutton="false" data-widget-editbutton="false" data-widget-deletebutton="false" data-widget-sortable="false">
+                        <header>
+                            <span class="widget-icon"><i class="fa fa-cog"></i></span>
+                            <h2>Triagem folha de ponto</h2>
+                        </header>
+                        <div>
+                            <div class="widget-body no-padding">
+                                <form action="javascript:gravar()" class="smart-form client-form" id="formUsuarioFiltro" method="post">
+                                    <div class="panel-group smart-accordion-default" id="accordion">
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                <h4 class="panel-title">
+                                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseFiltro" class="">
+                                                        <i class="fa fa-lg fa-angle-down pull-right"></i>
+                                                        <i class="fa fa-lg fa-angle-up pull-right"></i>
+                                                        Filtro
+                                                    </a>
+                                                </h4>
+                                            </div>
+                                            <div id="collapseFiltro" class="panel-collapse collapse in">
+                                                <div class="panel-body no-padding">
+                                                    <fieldset>
+                                                        <div class="row">
+                                                            <section class="col col-4 col-auto">
+                                                                <label class="label" for="funcionario">Funcionario</label>
+                                                                <label class="select">
+                                                                    <select id="funcionario" name="funcionario">
+                                                                        <option></option>
+                                                                        <?php
+                                                                        $reposit = new reposit();
+                                                                        $sql = "SELECT codigo, nome FROM 
+                                                                        Ntl.funcionario WHERE ativo = 1 ORDER BY nome";
+                                                                        $result = $reposit->RunQuery($sql);
+                                                                        foreach ($result as $row) {
+                                                                            $codigo = (int)$row['codigo'];
+                                                                            $nome = $row['nome'];
+                                                                            echo '<option value=' . $codigo . '>' . $nome . '</option>';
+                                                                        }
+                                                                        ?>
+                                                                    </select><i></i>
+                                                                </label>
+                                                            </section>
+                                                            <section class="col col-2 col-auto">
+                                                                <label for="mesAno" class="label">Mês/Ano</label>
+                                                                <label class="input">
+                                                                    <input id="mesAno" name="mesAno" type="text" data-dateformat="dd/mm/yy" class="datepicker" style="text-align: center" value="" data-mask="99/99/9999" data-mask-placeholder="-" autocomplete="off">
+                                                                </label>
+                                                            </section>
+                                                            <section class="col col-2 col-auto">
+                                                                <label class="label" for="status">Status</label>
+                                                                <label class="select">
+                                                                    <select id="status" name="status">
+                                                                        <option></option>
+                                                                        <?php
+                                                                        $reposit = new reposit();
+                                                                        $sql = "SELECT S.codigo,S.descricao from Ntl.status S  where S.ativo = 1 order by S.codigo";
+                                                                        $result = $reposit->RunQuery($sql);
+                                                                        foreach ($result as $row) {
+                                                                            $codigo = (int) $row['codigo'];
+                                                                            $descricao = $row['descricao'];
+                                                                            $pattern = "/^fechad(o|a)$/i";
+                                                                            if (preg_match($pattern, $descricao)) {
+                                                                                echo '<option value="' . $codigo . '" selected>' . $descricao . '</option>';
+                                                                            } else {
+                                                                                echo '<option value="' . $codigo . '">' . $descricao . '</option>';
+                                                                            }
+                                                                        }
+                                                                        ?>
+                                                                    </select><i></i>
+                                                                </label>
+                                                            </section>
+                                                    </fieldset>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <footer>
+                                        <button id="btnSearch" type="button" class="btn btn-primary pull-right" title="Buscar">
+                                            <span class="fa fa-search"></span>
+                                        </button>
+
+                                    </footer>
+                                </form>
+                            </div>
+                            <div id="resultadoBusca"></div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </section>
+        <!-- end widget grid -->
+    </div>
+    <!-- END MAIN CONTENT -->
+</div>
+<!-- END MAIN PANEL -->
+
+<!-- ==========================CONTENT ENDS HERE ========================== -->
+
+<!-- PAGE FOOTER -->
+<?php
+include("inc/footer.php");
+?>
+<!-- END PAGE FOOTER -->
+
+<?php
+//include required scripts
+include("inc/scripts.php");
+?>
+<!--script src="<?php echo ASSETS_URL; ?>/js/businessTabelaBasica.js" type="text/javascript"></script-->
+<!-- PAGE RELATED PLUGIN(S) 
+<script src="..."></script>-->
+<!-- Flot Chart Plugin: Flot Engine, Flot Resizer, Flot Tooltip -->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.cust.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.resize.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.time.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.tooltip.min.js"></script>
+
+<!-- Vector Maps Plugin: Vectormap engine, Vectormap language -->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+
+<!-- Full Calendar -->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/moment/moment.min.js"></script>
+<!--<script src="/js/plugin/fullcalendar/jquery.fullcalendar.min.js"></script>-->
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/fullcalendar/fullcalendar.js"></script>
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/fullcalendar/locale-all.js"></script>
+
+
+<script>
+    $(document).ready(function() {
+
+        listarFiltro();
+
+        $('#btnSearch').on("click", function() {
+            listarFiltro();
+        });
+
+        $("#mesAno").on("change", function() {
+            let data = $("#mesAno").val();
+            if(!data){
+                return
+            }
+            data = data.replace(/^\d{2}/g,"01")
+           return $("#mesAno").val(data);
+        });
+
+    });
+
+    function listarFiltro() {
+        var funcionario = $('#funcionario').val();
+        debugger;
+        var mesAno = $('#mesAno').val();
+        if(mesAno){
+        const aux = mesAno.split('/');
+        mesAno = aux[2]+"-"+aux[1]+"-"+aux[0];
+        }
+        var status = $('#status').val();
+        
+        $('#resultadoBusca').load('funcionario_triagemFolhaDePontoListagemGestor.php?', {
+            funcionario,
+            mesAno,
+            status
+        });
+    }
+</script>
